f7fa0b94b73346685169ce5797e4d1634ea95514
Derek Leung <derek.s.leung.407@gmail.com>
2026-05-17 19:37:54 -0700

    fix(web): make chart style setting safer

--- apps/web-frontend/src/shared/components/base/chart.tsx
+++ apps/web-frontend/src/shared/components/base/chart.tsx
@@ -81,26 +81,22 @@ const ChartStyle = ({ id, config }: { id: string; config: ChartConfig }) => {
     return null;
   }
 
-  return (
-    <style
-      dangerouslySetInnerHTML={{
-        __html: Object.entries(THEMES)
-          .map(
-            ([theme, prefix]) => `
-${prefix} [data-chart=${id}] {
-${colorConfig
-  .map(([key, itemConfig]) => {
-    const color = itemConfig.theme?.[theme as keyof typeof itemConfig.theme] ?? itemConfig.color;
-    return color ? `  --color-${key}: ${color};` : null;
-  })
-  .join("\n")}
-}
-`,
-          )
-          .join("\n"),
-      }}
-    />
-  );
+  const chartStyle = Object.entries(THEMES)
+    .map(
+      ([theme, prefix]) => `
+        ${prefix} [data-chart=${id}] {
+        ${colorConfig
+          .map(([key, itemConfig]) => {
+            const color = itemConfig.theme?.[theme as keyof typeof itemConfig.theme] ?? itemConfig.color;
+            return color ? `  --color-${key}: ${color};` : null;
+          })
+          .join("\n")}
+        }
+      `,
+    )
+    .join("\n");
+
+  return <style>{chartStyle}</style>;
 };
 
 const ChartTooltip = RechartsPrimitive.Tooltip;
