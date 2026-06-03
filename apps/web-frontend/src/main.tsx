c9886c1e8476ba5eb34f055e7f7b71ca18fbd172
Derek Leung <derek.s.leung.407@gmail.com>
2026-06-02 19:40:57 -0700

    chore(web): wire tanstack query devtools

--- apps/web-frontend/src/main.tsx
+++ apps/web-frontend/src/main.tsx
@@ -18,6 +18,17 @@ declare module '@tanstack/react-router' {
   }
 }
 
+// This code is only for TypeScript
+declare global {
+  interface Window {
+    __TANSTACK_QUERY_CLIENT__:
+      import('@tanstack/query-core').QueryClient
+  }
+}
+
+// This code is for all users
+window.__TANSTACK_QUERY_CLIENT__ = queryClient;
+
 const rootElement = document.getElementById('app')!
 
 if (!rootElement.innerHTML) {
